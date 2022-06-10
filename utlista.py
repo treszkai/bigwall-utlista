34bb058be25a11d81551b80f74f9252dea9d4872
Laszlo Treszkai <laszlo.treszkai@gmail.com>
2022-06-10 15:07:02 +0200

    fix: don't print double-newlines
    This probably fails on Windows but so be it.

--- utlista.py
+++ utlista.py
@@ -10,7 +10,7 @@ routes = [
     {
         "id": el.attrs["data-number"],
         "french-grade": el.attrs["data-french-grade"],
-        "title": el.attrs["data-title"],
+        "title": el.attrs["data-title"].strip(),
     }
     for el in sorted(route_elements, key=lambda x: x.attrs["data-number"])
 ]
@@ -19,7 +19,7 @@ if not routes:
     print('Hoppa, egy utat se talaltam a valaszban, lehuztak a rolot vagy mi a fasz van')
     sys.exit(1)
 
-writer = csv.DictWriter(sys.stdout, fieldnames=['id', 'french-grade', 'title'])
+writer = csv.DictWriter(sys.stdout, fieldnames=['id', 'french-grade', 'title'], lineterminator='\n')
 writer.writeheader()
 for route in routes:
     writer.writerow(route)
